c36f02327c5af7c2e34c919d2c54636cb6907eed
SaloniMadhan <69685422+SaloniMadhan@users.noreply.github.com>
2020-09-22 00:13:41 +0530

    Update SenderTest.cpp

--- CaseStudy/SenderTest/SenderTest.cpp
+++ CaseStudy/SenderTest/SenderTest.cpp
@@ -10,23 +10,18 @@ using namespace std;
 
 TEST_CASE("When a non exiting file is received by Sender then it prints file does not exist") {
 	string FileName="../FootFall.csv";
-	string s = ReadAndCheckInputFile(FileName);
+	string s = ReadInputFileAndPrint(FileName);
 	REQUIRE( s== "File does not exist");
 }
 
 TEST_CASE("When file received by Sender contains Invalid data then it prints File has some Invalid Data on console") {
 	string FileName = "../TestFootfallData.csv";
-	string s = ReadAndCheckInputFile(FileName);
+	string s = ReadInputFileAndPrint(FileName);
 	REQUIRE(s == "File has some Invalid Data");
 }
 
-TEST_CASE("When file received by Sender contains valid data then it converts CSV file to Json data and prints it on console") {
+TEST_CASE("When file received by Sender contains valid data then it converts CSV data in a vector structure on console") {
 	string FileName = "../FootFallData.csv";
-	string str = ReadAndCheckInputFile(FileName);
-	string s;
-	if (str == "File Read successfully")
-		s = ConvertToJson(FileName);
-	REQUIRE(s == "File converted successfully");
+	string str = ReadInputFileAndPrint(FileName);
+	REQUIRE(str == "File read and printed on console successfully");
 }
-
-
