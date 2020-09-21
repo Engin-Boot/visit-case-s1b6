87e253c9860be14734fdf751b341ff6d4dd9f38b
SaloniMadhan <69685422+SaloniMadhan@users.noreply.github.com>
2020-09-22 00:11:52 +0530

    Update Sender.h

--- CaseStudy/Sender/Sender.h
+++ CaseStudy/Sender/Sender.h
@@ -3,10 +3,10 @@
 #include<string.h>
 #include<fstream>
 #include <sstream>
-#include "../jsoncons-0.156.0/include/jsoncons/json.hpp"
-#include "../jsoncons-0.156.0/include/jsoncons/jsoncons_ext/csv/csv.hpp"
+//#include "../jsoncons-0.156.0/include/jsoncons/json.hpp"//
+//#include "../jsoncons-0.156.0/include/jsoncons/jsoncons_ext/csv/csv.hpp"
 
-using namespace jsoncons;
+//using namespace jsoncons;
 using namespace std;
 
 //Returns true if s is a number else false
@@ -26,7 +26,7 @@ bool is_file_open(string FileName)
 	return f.good();
 }
 
-string ReadAndCheckInputFile(string FileName)
+/*string ReadAndCheckInputFile(string FileName)
 {
 	bool flag = true;
 	string s;
@@ -86,4 +86,85 @@ string ConvertToJson(string CSVFileName)
 	ojson tasks = csv::decode_csv<ojson>(is, options);
 	std::cout << "\n" << pretty_print(tasks) << "\n\n";
 	return "CSV File converted to Json format";
+}*/
+vector <vector<string>> ReadInputFile(string FileName)
+{
+	ifstream csvFile;
+	csvFile.open(FileName);
+		string line;
+		getline(csvFile, line); // skip the 1st line
+	
+		vector <vector<string>> record;
+		while (getline(csvFile, line))
+		{
+			if (line.empty()) // skip empty lines:
+			{
+				cout << "empty line!" << endl;
+				continue;
+			}
+			istringstream iss(line);
+			string lineStream;
+			vector <string> row;
+			while (getline(iss, lineStream, ','))
+			{
+				row.push_back(lineStream);
+			}
+			record.push_back(row);
+			
+			//return "CSV File converted to a vector structure";
+		}
+		
+		csvFile.close();
+	
+		return record;
+}
+
+string checkInputData(vector <vector<string>> rec)
+{
+	bool flag = true;
+	for (std::vector<string> vec : rec)
+	{
+		for (string rowdata : vec)
+		{
+			if (!checkEachEntry(rowdata))
+			{
+				flag = false;
+				return "File has Invalid Data";
+			}	
+		}
+	}
+	if (flag == true)
+	{
+		return "File has valid data";
+	}
+	return " ";
+}
+
+void printFootFallData(vector <vector<string>> record)
+{
+	cout << "id,Hrs,min,sec,day,date,month,year,Count\n";
+	for (int i = 0; i < record.size(); i++) {
+		for (int j = 0; j < record[i].size(); j++)
+			cout << record[i][j] << ",";
+		cout << endl;
+	}
+}
+
+string ReadInputFileAndPrint(string FileName)
+{
+	if (is_file_open(FileName))
+	{
+		vector <vector<string>> record = ReadInputFile(FileName);
+		string res = checkInputData(record);
+		if (res == "File has valid data")
+		{
+			printFootFallData(record);
+			return "File read and printed on console successfully";
+		}
+		else
+			return "File has invalid Data";
+	}
+	else
+		return "File does not exist";	
+	return " ";
 }
