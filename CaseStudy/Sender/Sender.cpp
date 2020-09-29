081fc35ec2367f39f0feb8f59bff0ccc938e42da
SaloniMadhan <69685422+SaloniMadhan@users.noreply.github.com>
2020-09-29 11:56:18 +0530

    Update Sender.cpp

--- CaseStudy/Sender/Sender.cpp
+++ CaseStudy/Sender/Sender.cpp
@@ -5,7 +5,7 @@ using namespace std;
  
 int main()
 {
-	string FileName = "../FootfallData.csv";
+	string FileName = "../FootFallData.csv";
 	string s=ReadInputFileAndPrint(FileName);
 	if (s != "File read and printed on console successfully") //else prints file data
 		cout << s;
